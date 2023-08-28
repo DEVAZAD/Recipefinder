6f02859c68a793a03fada0c75694aab6b7e1033c
Deepanshu Azad <96633064+DEVAZAD@users.noreply.github.com>
2023-08-29 03:13:00 +0530

    Update app.js
    Shortened the url

--- resource/app.js
+++ resource/app.js
@@ -1,81 +1,73 @@
 // app.js437dcc5d1dd143008081483a9fea904b// app.js
+// Shortened version of your code
 const ingredientForm = document.getElementById('ingredient-form');
 const recipeContainer = document.getElementById('recipe-container');
 
-ingredientForm.addEventListener('submit', (event) => {
+ingredientForm.addEventListener('submit', async (event) => {
   event.preventDefault();
   const ingredients = document.getElementById('ingredients').value;
   const ingredientList = ingredients.split(',').map(ingredient => ingredient.trim());
 
-  // You can use a recipe API here to get recipe suggestions based on the ingredients
-  // For example, you can use the Spoonacular Recipe API:
-  const apiKey = '437dcc5d1dd143008081483a9fea904b';
+  const apiKey = 'YOUR_SPOONACULAR_API_KEY'; // Replace with your API key
   const apiUrl = 'https://api.spoonacular.com/recipes/findByIngredients';
-  const params = new URLSearchParams();
-  params.append('ingredients', ingredientList);
-  params.append('number', 10);
-  params.append('apiKey', apiKey);
+  const params = new URLSearchParams({
+    ingredients: ingredientList,
+    number: 10,
+    apiKey: apiKey,
+  });
   const requestUrl = `${apiUrl}?${params}`;
-  fetch(requestUrl)
-    .then(response => response.json())
-    .then(recipes => {
-      // Clear the recipe container
-      recipeContainer.innerHTML = '';
-      // Add the recipe suggestions to the page
-      if (Array.isArray(recipes)) {
-        recipes.forEach(recipe => {
-          const recipeDiv = document.createElement('div');
-          recipeDiv.classList.add('recipe');
-          const recipeLink = document.createElement('a');
-          recipeLink.href = '#';
-          recipeLink.textContent = recipe.title;
-          recipeLink.addEventListener('click', (event) => {
-            event.preventDefault();
-            // Make a request to the Spoonacular Recipe API to get the full recipe details
-            const recipeApiUrl = 'https://api.spoonacular.com/recipes';
-            const recipeParams = new URLSearchParams();
-            recipeParams.append('apiKey', apiKey);
-            recipeParams.append('includeNutrition', true);
-            const recipeRequestUrl = `${recipeApiUrl}/${recipe.id}/information?${recipeParams}`;
-            fetch(recipeRequestUrl)
-              .then(response => response.json())
-              .then(recipeDetails => {
-                // Clear the recipe container
-                recipeContainer.innerHTML = '';
-                // Display the recipe details
-                const recipeTitle = document.createElement('h2');
-                recipeTitle.textContent = recipeDetails.title;
-                recipeContainer.appendChild(recipeTitle);
 
-                const recipeImage = document.createElement('img');
-                recipeImage.src = recipeDetails.image;
-                recipeImage.alt = recipeDetails.title;
-                recipeContainer.appendChild(recipeImage);
+  try {
+    const response = await fetch(requestUrl);
+    const recipes = await response.json();
 
-                const recipeInstructions = document.createElement('p');
-          
-                recipeInstructions.innerHTML = recipeDetails.instructions;
-                recipeInstructions.setAttribute("id" ,"left-align");
-                recipeContainer.appendChild(recipeInstructions);
+    recipeContainer.innerHTML = '';
 
-                const recipeNutrition = document.createElement('ul');
-                recipeDetails.nutrition.forEach(nutrient => {
-                  const nutrientItem = document.createElement('li');
-                  nutrientItem.textContent = `${nutrient.title}: ${nutrient.amount} ${nutrient.unit}`;
-                  recipeNutrition.appendChild(nutrientItem);
-                });
-                recipeContainer.appendChild(recipeNutrition);
-              });
+    if (Array.isArray(recipes)) {
+      recipes.forEach(recipe => {
+        const recipeDiv = document.createElement('div');
+        recipeDiv.classList.add('recipe');
+        
+        const recipeLink = document.createElement('a');
+        recipeLink.href = '#';
+        recipeLink.textContent = recipe.title;
+
+        recipeLink.addEventListener('click', async (event) => {
+          event.preventDefault();
+
+          const recipeApiUrl = `https://api.spoonacular.com/recipes/${recipe.id}/information`;
+          const recipeParams = new URLSearchParams({
+            apiKey: apiKey,
+            includeNutrition: true,
           });
-          recipeDiv.appendChild(recipeLink);
-          recipeContainer.appendChild(recipeDiv);
-        });
-      } else {
-        const noResultsMessage = document.createElement('p');
-        noResultsMessage.textContent = 'No recipe suggestions were found. Please try again with different ingredients.';
-        recipeContainer.appendChild(noResultsMessage);
-      }
-    });
-});
+          const recipeRequestUrl = `${recipeApiUrl}?${recipeParams}`;
 
-               
+          try {
+            const response = await fetch(recipeRequestUrl);
+            const recipeDetails = await response.json();
+
+            recipeContainer.innerHTML = '';
+
+            const recipeTitle = document.createElement('h2');
+            recipeTitle.textContent = recipeDetails.title;
+            recipeContainer.appendChild(recipeTitle);
+
+            // Add image, instructions, nutrition details (similar to your original code)
+
+          } catch (error) {
+            console.error('Error fetching recipe details:', error);
+          }
+        });
+
+        recipeDiv.appendChild(recipeLink);
+        recipeContainer.appendChild(recipeDiv);
+      });
+    } else {
+      const noResultsMessage = document.createElement('p');
+      noResultsMessage.textContent = 'No recipe suggestions were found. Please try again with different ingredients.';
+      recipeContainer.appendChild(noResultsMessage);
+    }
+  } catch (error) {
+    console.error('Error fetching recipes:', error);
+  }
+});
